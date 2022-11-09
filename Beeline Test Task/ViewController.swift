0ee430b074918669f2050a663be5dfda6b3e1e8b
Eliah-A <andrusyak1s@yandex.ru>
2022-11-09 15:30:12 +0600

    Initial Commit

---
+++ Beeline Test Task/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Beeline Test Task
+//
+//  Created by Илья Сергеевич on 09.11.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
